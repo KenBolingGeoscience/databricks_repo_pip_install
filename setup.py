649be1ff27a9aa2c38c64bbf4bb5c583467ed172
Ken Boling <KenBoling.Geoscience@gmail.com>
2022-07-08 21:52:15 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='foo',
-   version='1.0',
+   version='1.1',
    description='A useful module',
    author='Man Foo',
    author_email='foomail@foo.example',
